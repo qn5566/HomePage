f67f5684611d458aa357b3dd51a4e73368f8d243
Hsu <sohotguys@gmail.com>
2016-01-05 17:12:59 +0800

    新增動態展示

---
+++ js/001.js
@@ -0,0 +1,54 @@
+/**
+ * Created by heran on 2016/1/4.
+ */
+$(function(){
+
+    $("#goTop").click(function(){
+
+        $("html,body").animate({scrollTop:0},1200);
+
+        return false;
+
+
+    });
+    $("#About").click(function(){
+
+        $("html,body").animate({scrollTop:400},1200);
+
+        return false;
+
+
+    });
+    $("#Skills").click(function(){
+
+        $("html,body").animate({scrollTop:10},1200);
+
+        return false;
+
+
+    });
+    $("#Project").click(function(){
+
+        $("html,body").animate({scrollTop:10},1200);
+
+        return false;
+
+
+    });
+    $("#Career").click(function(){
+
+        $("html,body").animate({scrollTop:10},1200);
+
+        return false;
+
+
+    });
+    $("#Eduction").click(function(){
+
+        $("html,body").animate({scrollTop:10},1200);
+
+        return false;
+
+
+    });
+});
